9a83029f652cf66eec4233c641455054fae303c4
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 01:49:59 +0200

    first test for the print array method

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(i);
+            if (i < size - 1)
+                Console.Write(" ");
+        }
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else
+        {
+            Console.WriteLine();
+            return new int[size];
+        }
+    }
+}
