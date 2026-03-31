3d4fcd9dfdd4724a855d61ee6b52b9656a18d1d4
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 23:59:21 +0200

    first try for the print numbers 0 to 99 script

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 99; i++)
+            if (i != 99)
+                Console.Write(i.ToString("D2") + ", ");
+            else
+                Console.WriteLine(i.ToString("D2"));
+    }
+}
