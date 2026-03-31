a7e70e27df8671ef1eccbbc7068bbeca4fa92252
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 00:23:05 +0200

    first try testing the script who print all different combinations

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int two, one;
+        for (two = 0; two <= 8; two++)
+        {
+            for (one = two + 1; one <= 9; one++)
+            {
+                Console.Write($"{two}{one}");
+                if (!(two == 8 && one == 9))
+                    Console.Write(", ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
