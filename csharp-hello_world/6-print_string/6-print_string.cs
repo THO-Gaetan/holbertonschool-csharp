fd64aea1ddeee8394db8ebd5a7ba677570db0cc7
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 12:58:08 +0200

    First try Printing a stored string

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine("{0}{0}{0}", str);
+        Console.WriteLine(str.Substring(0, 9));
+        }
+}
