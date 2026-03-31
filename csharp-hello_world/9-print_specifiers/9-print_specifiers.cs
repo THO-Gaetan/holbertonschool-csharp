a0b7e26b59f4556b7513e6001220cfa45a9800ad
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 13:23:16 +0200

    first try printing % & currency variable

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0:0.00%}", percent);
+        Console.WriteLine("Currency: {0:$#,##0.00}", currency);
+	}
+}
