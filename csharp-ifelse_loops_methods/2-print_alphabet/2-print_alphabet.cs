180452e983de21eacbb936a675b89754a74f564c
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 19:49:30 +0200

    first time using a boucle to print the alphabet

--- csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char c = 'a'; c <= 'z' ; c++)
+            Console.Write(c);
+    }
+}
