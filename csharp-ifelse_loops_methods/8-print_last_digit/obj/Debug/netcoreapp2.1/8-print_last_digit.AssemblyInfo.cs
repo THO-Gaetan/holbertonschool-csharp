89e4353d04ecb1316e4cb60fb36cafdc909d5265
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 00:51:33 +0200

    removing the main.cs

--- csharp-ifelse_loops_methods/8-print_last_digit/obj/Debug/netcoreapp2.1/8-print_last_digit.AssemblyInfo.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/obj/Debug/netcoreapp2.1/8-print_last_digit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a85fffdf0a580bebef6cbd0fe4e5b6baa1cf74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4db16fb3beb3dbecdd8cb4c92d789515e858c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("8-print_last_digit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
