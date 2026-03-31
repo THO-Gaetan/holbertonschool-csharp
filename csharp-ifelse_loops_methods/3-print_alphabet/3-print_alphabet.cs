02968d066def08bbab0fb6749029fea03a7bdefa
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 20:40:28 +0200

    first try writing the alphabet & removing 2 letter

--- csharp-ifelse_loops_methods/3-print_alphabet/3-print_alphabet.cs
+++ csharp-ifelse_loops_methods/3-print_alphabet/3-print_alphabet.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char c = 'a'; c <= 'z' ; c++)
+            if (c != 'q' && c != 'e')
+                Console.Write(c);
+            else
+                Console.Write("");
+    }
+}
