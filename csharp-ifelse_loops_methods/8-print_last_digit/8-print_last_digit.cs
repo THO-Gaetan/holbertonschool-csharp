0a4db16fb3beb3dbecdd8cb4c92d789515e858c5
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 00:46:38 +0200

    first time testing the method that get the last digit of a number

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int LastDigit = number % 10;
+        if (LastDigit < 0)
+            LastDigit = LastDigit * -1;
+        Console.Write(LastDigit);
+        return LastDigit;
+    }
+}
