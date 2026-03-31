27ebe39c82fe8bc5f8dab8c24cb6946a9040c85c
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 20:42:24 +0200

    modify wrong files name

--- csharp-ifelse_loops_methods/3-print-alphabt.sh
+++ csharp-ifelse_loops_methods/3-print-alphabt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-dotnet new console -n "3-print_alphabet"
-cd 3-print_alphabet
+dotnet new console -n "3-print_alphabt"
+cd 3-print_alphabt
 dotnet build
-mv Program.cs 3-print_alphabet.cs
+mv Program.cs 3-print_alphabt.cs
 dotnet run
