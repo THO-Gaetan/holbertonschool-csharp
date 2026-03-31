877e7babf892873c1feefef83f703f131adda954
THO-Gaetan <9771@holbertonstudents.com>
2026-03-31 12:42:55 +0200

    first try to use a stored variable

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		int number = 972;
+		Console.WriteLine("{0} Mission Street", number);
+	}
+}
