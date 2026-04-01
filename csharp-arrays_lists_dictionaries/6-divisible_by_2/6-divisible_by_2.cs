a29923b6039d17d37dd1a7327bbbd0f5b87f58c7
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 11:42:07 +0200

    first try for module to find if an int is a multiple of 2

--- csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ListInt = System.Collections.Generic.List<int>;
+using ListBool = System.Collections.Generic.List<bool>;
+
+class List
+{
+    public static ListBool DivisibleBy2(ListInt myList)
+    {
+        if (myList.Count == 0)
+        {
+            Console.WriteLine("List is empty");
+            return new ListBool();
+        }
+        ListBool result = new ListBool();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+                result.Add(true);
+            else
+                result.Add(false);
+        }
+        return result;
+    }
+}
