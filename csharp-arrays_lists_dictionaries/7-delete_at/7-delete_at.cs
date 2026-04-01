4cf7eaad7499717e9297b588fce78ab0a7101d7f
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 11:59:52 +0200

    first try for the method to delete an element in a list

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ListInt = System.Collections.Generic.List<int>;
+
+class List
+{
+    public static ListInt DeleteAt(ListInt myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index out of range");
+            return myList;
+        }
+        myList.RemoveAt(index);
+        return myList;
+    }
+}
