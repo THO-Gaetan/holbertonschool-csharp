c6a845537140341be37c21cb4d50d43b077e25d6
THO-Gaetan <9771@holbertonstudents.com>
2026-04-01 02:10:31 +0200

    first try of my method to create a List of int

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ListInt = System.Collections.Generic.List<int>;
+
+class List
+{
+    public static ListInt CreatePrint(int size)
+    {
+        ListInt myList = new ListInt();
+        if (size <= 0)
+        {
+            Console.WriteLine();
+            return myList;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            myList.Add(i);
+            if (i == size - 1)
+                Console.WriteLine(myList[i]);
+            else
+                Console.Write(myList[i] + " ");
+        }
+        return myList;
+    }
+}
