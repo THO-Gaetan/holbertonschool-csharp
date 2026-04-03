7a538db0b72bbf8e98c383697cfa52ddc36f6110
THO-Gaetan <9771@holbertonstudents.com>
2026-04-03 08:32:00 +0200

    first time testing the two dimensional print script

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -2,7 +2,18 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
+    static void Main(string[] args)
+    {
+        int[,] arr = new int[5, 5];
+        arr[2, 2] = 1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
