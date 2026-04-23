f80b9f3e98e3e4992410126d4ba5a9f838afee72
THO-Gaetan <9771@holbertonstudents.com>
2026-04-23 16:50:24 +0200

    adding an exception if the given list are empty

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -6,28 +6,21 @@ public class List
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
         List<int> result = new List<int>();
-        try 
+        for (int i = 0; i < listLength; i++)
         {
-            for (int i = 0; i < listLength; i++)
+            try
             {
-                try
-                {
-                    result.Add(list1[i] / list2[i]);
-                }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Cannot divide by zero");
-                    result.Add(0);
-                }
+                result.Add(list1[i] / list2[i]);
             }
-        }
-        catch (ArgumentNullException)
-        {
-            Console.WriteLine("Null list");
-        }
-        catch (ArgumentOutOfRangeException)
-        {
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+                result.Add(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
             Console.WriteLine("Out of range");
+            }
         }
         return result;
     }
