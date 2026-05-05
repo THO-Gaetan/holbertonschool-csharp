433cddd74e45cef2acf6792173cc2a4d46291184
THO-Gaetan <9771@holbertonstudents.com>
2026-05-05 14:13:04 +0200

    test of the first exercice of the project

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Obj
+{
+    public static bool IsOfTypeInt(object obj)
+    {
+        Type gettype = obj.GetType();
+        if (gettype == typeof(int))
+            return true;
+        return false;
+    }
+}
