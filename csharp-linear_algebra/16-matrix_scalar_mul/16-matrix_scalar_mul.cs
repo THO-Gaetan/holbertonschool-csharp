b867c5520c6fadbea963bde028acaa0b07fbb349
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:52:17 +0200

    fixing wrong logic connection

--- csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -4,8 +4,8 @@ class MatrixMath
 {
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        if (matrix.GetLength(0) != 2 && matrix.GetLength(0) !=3)
-        || (matrix.GetLength(1) != 2 && matrix.GetLength(1) !=3)
+        if ((matrix.GetLength(0) != 2 && matrix.GetLength(0) !=3)
+        || (matrix.GetLength(1) != 2 && matrix.GetLength(1) !=3))
             return double[,] { -1 };
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
