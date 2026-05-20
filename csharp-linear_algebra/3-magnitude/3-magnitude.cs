128ae37ceec8392611375aafe64443c058856d4a
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 03:46:53 +0200

    calculate a vector length

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/// <summary>Class Vector</summary>
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.length != 2 && vector.length != 3)
+            return -1;
+        double sum = 0;
+        foreach (double num in vector)
+            sum += Math.Pow(num, 2);
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
