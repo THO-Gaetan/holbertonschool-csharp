23acc7fcaa8abbac0149c3ce942206a41850aa80
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:13:28 +0200

    return a double instead of a int

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -6,7 +6,7 @@ class VectorMath
     {
         if (vectorA.Length != vectorB.Length && vectorA.length != 2 && vectorA.Length != 3)
             return new double[] { -1 };
-        int result = 0;
+        double result = 0;
         for (int i = 0; i < vectorA.Length; i++)
             result += vectorA[i] * vectorB[i];
         return result;
