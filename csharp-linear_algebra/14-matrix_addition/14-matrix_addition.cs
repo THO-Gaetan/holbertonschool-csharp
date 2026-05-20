d998cba6bce71b066a854f36bcf469224482b280
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:42:43 +0200

    adding the right condition to return -1)

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -4,7 +4,8 @@ class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        if ((matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        || ((matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3) || (matrix1.GetLength(1) !=2 && matrix1.GetLength(1) !=3)))
             return new double[,] { { -1 } };
          double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
          for (int i = 0; i < matrix1.GetLength(0); i++)
