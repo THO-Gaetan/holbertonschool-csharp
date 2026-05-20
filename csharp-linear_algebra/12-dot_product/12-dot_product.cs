981a02a3fc9a72c585e4a24da9013ff813b02bff
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:15:09 +0200

    fixing the return type

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -6,9 +6,9 @@ class VectorMath
     {
         if (vectorA.Length != vectorB.Length && vectorA.Length != 2 && vectorA.Length != 3)
             return new double[] { -1 };
-        double result = 0;
+        double[] result = new double[vectorA.Length];
         for (int i = 0; i < vectorA.Length; i++)
-            result += vectorA[i] * vectorB[i];
+            result[i] = vectorA[i] * vectorB[i];
         return result;
     }
 }
