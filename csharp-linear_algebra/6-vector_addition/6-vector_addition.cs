5fc0269134d0dc65d17225efe3379dc226853028
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 04:04:54 +0200

    adding xml doc

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,8 +1,17 @@
-﻿using System;
-
-/// <summary>Class Vector</summary>
+﻿/// <summary>
+/// Provides vector math operations for 2D and 3D vectors.
+/// </summary>
 class VectorMath
 {
+    /// <summary>
+    /// Adds two vectors of the same size together component by component.
+    /// </summary>
+    /// <param name="vector1">The first vector, must be 2D or 3D.</param>
+    /// <param name="vector2">The second vector, must be 2D or 3D.</param>
+    /// <returns>
+    /// A new vector that is the sum of two vector.
+    /// Returns -1 if the vectors are different size or not 2D or 3D.
+    /// </returns>
     public static double[] Add(double[] vector1, double[] vector2)
     {
         if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3))
