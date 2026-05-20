e045dcfa2b05765c24f7b58b52486c835c494f0d
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:12:46 +0200

    calculate the dot of two vector

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class VectorMath
+{
+    public static double DotProduct(double[] vectorA, double[] vectorB)
+    {
+        if (vectorA.Length != vectorB.Length && vectorA.length != 2 && vectorA.Length != 3)
+            return new double[] { -1 };
+        int result = 0;
+        for (int i = 0; i < vectorA.Length; i++)
+            result += vectorA[i] * vectorB[i];
+        return result;
+    }
+}
