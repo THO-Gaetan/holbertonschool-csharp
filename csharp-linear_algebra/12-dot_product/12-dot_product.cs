3a7dbe60b7eb4e9784bcdb8692b24453b0d6b05f
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 12:18:30 +0200

    fixing wrong type use for negative return

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -4,11 +4,11 @@ class VectorMath
 {
     public static double DotProduct(double[] vectorA, double[] vectorB)
     {
-        if (vectorA.Length != vectorB.Length && vectorA.Length != 2 && vectorA.Length != 3)
-            return new double[] { -1 };
-        double[] result = new double[vectorA.Length];
+        if (vectorA.Length != vectorB.Length || (vectorA.Length != 2 && vectorA.Length != 3))
+            return -1;
+        double result = 0;
         for (int i = 0; i < vectorA.Length; i++)
-            result[i] = vectorA[i] * vectorB[i];
+            result += vectorA[i] * vectorB[i];
         return result;
     }
 }
