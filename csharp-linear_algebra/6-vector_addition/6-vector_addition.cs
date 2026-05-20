82568040359e84a6bc4b61f87ee91dd7b0efc9d3
THO-Gaetan <9771@holbertonstudents.com>
2026-05-20 04:01:57 +0200

    changing the null to -1 on fail test

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -6,7 +6,7 @@ class VectorMath
     public static double[] Add(double[] vector1, double[] vector2)
     {
         if (vector1.Length != vector2.Length || (vector1.Length != 2 && vector1.Length != 3))
-            return null;
+            return new double[] { -1 };
         double[] result = new double[vector1.Length];
         for (int i = 0; i < vector1.Length; i++)
         {
