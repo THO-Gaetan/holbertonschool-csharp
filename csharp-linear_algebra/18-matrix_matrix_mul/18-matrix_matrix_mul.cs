effd2685c7c887b8e4de0f3f4668240ee965e41b
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 12:11:24 +0200

    wrong cols count

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -8,7 +8,7 @@ class MatrixMath
         || ((matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3) || (matrix1.GetLength(1) != 2 && matrix1.GetLength(1) != 3)))
             return new double[,] { { -1 } };
         int rows = matrix1.GetLength(0);
-        int cols = matrix1.GetLength(1);
+        int cols = matrix2.GetLength(1);
         double[,] result = new double[rows, cols];
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < cols; j++)
