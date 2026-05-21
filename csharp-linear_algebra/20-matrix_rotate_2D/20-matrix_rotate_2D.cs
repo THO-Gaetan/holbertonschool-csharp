17eae61babbd633f8073c5aa95a669619c1a7723
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 12:25:41 +0200

    fixing wrong typo

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -26,7 +26,7 @@ class MatrixMath
         
         for (int i = 0; i  < size; i++)
             for (int j = 0; j < size; j++)
-                result[i,j] = Math.Round(result(i, j), 0);
+                result[i, j] = Math.Round(result[i, j], 0);
         return result;
     }
 }
