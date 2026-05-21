b4fc26b769b508a28d2303f0c3075f1dabb273ce
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 13:43:23 +0200

    exercice 26

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -12,7 +12,7 @@ class MatrixMath
         if (size == 2)
         {
             return (matrix[0,0] * matrix[1,1]) -
-                   (matrix[0,1] * matrix[1,0]) 
+                   (matrix[0,1] * matrix[1,0]);
         }
         else
         {
