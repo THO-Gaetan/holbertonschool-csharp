ad1b90e62794507a2442d4b72f0cf2666bbf8935
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 12:52:15 +0200

    shear a square 2D

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1) || (matrix.GetLength(0) != 2))
+        || (direction != 'x' && direction != 'y')
+            return new double[,] { { -1 } };
+
+        double[,] shear = { { 1, 0 }, { 0, 1 } };
+        if (direction == "x")
+            shear[0, 1] = factor;
+        else
+            shear[1, 0] = factor;
+
+        int size = matrix.GetLength(0);
+        double[,] result = new double[size, size];
+        
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = 0; k < size; k++)
+                    result[i, j] += matrix[i, k] * shear[k, j];
+
+        return result;
+    }
+}
