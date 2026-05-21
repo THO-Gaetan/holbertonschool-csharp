3b815cfcb2abd7797922ad822fbbc21b2fc1f77a
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 11:45:23 +0200

    adding exercice 18

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        if ((matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+        || ((matrix1.GetLength(0) != 2 && matrix1.GetLength(0) != 3) || (matrix1.GetLength(1) !=2 && matrix1.GetLength(1) !=3)))
+            return new double[,] { { -1 } };
+        int rows = matrix1.GetLength(0);
+        int cols = matrix1.GetLength(1);
+        double[,] result = new double[rows, cols];
+        if (matrix1.GetLength == 2)
+        {
+            result[0,0] = (matrix1[0,0] x matrix2[0,0]) + (matrix1[0,1] * matrix2[1,0])
+            result[0,1] = (matrix1[0,0] x matrix2[0,1]) + (matrix1[0,1] * matrix2[1,1])
+            result[1,0] = (matrix1[1,0] x matrix2[0,0]) + (matrix1[1+1] * matrix2[1,0])
+            result[1,1] = (matrix1[1,0] x matrix2[0,1]) + (matrix1[1+1] * matrix2[1,1])
+        }
+        return result;
+    }
+}
