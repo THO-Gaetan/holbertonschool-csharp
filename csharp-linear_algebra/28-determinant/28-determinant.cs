2dbeab21f844ee985f543546529bd72f93af8a8d
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 13:42:25 +0200

    exercice 26

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double Determinant(double[,] matrix)
+    {
+        int size = matrix.GetLength(0);
+
+        if (matrix.getLength(0) != matrix.getLength(1) || (size != 2 && size != 3))
+            return -1;
+        
+        if (size == 2)
+        {
+            return (matrix[0,0] * matrix[1,1]) -
+                   (matrix[0,1] * matrix[1,0]) 
+        }
+        else
+        {
+            double result1 = matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]));
+            double result2 = matrix[0, 1] * ((matrix[1, 0] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 0]));
+            double result3 = matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]));
+
+            return result1 - result2 + result3;
+        }
+    }
+}
