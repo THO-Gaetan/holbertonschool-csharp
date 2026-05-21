3e48b7cf48c601b2c37010d3d724c00214468529
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 14:12:19 +0200

    exercice 31

--- csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] { { -1 } };
+
+        double det = (matrix[0, 0] * matrix[1, 1]) -
+                     (matrix[0, 1] * matrix[1, 0]);
+
+        if (det == 0)
+            return new double[,] { { -1 } };
+
+        double[,] result = {
+            {  matrix[1, 1], -matrix[0, 1] },
+            { -matrix[1, 0],  matrix[0, 0] }
+        };
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                result[i, j] = Math.Round(result[i, j] / det, 2);
+
+        return result;
+    }
+}
