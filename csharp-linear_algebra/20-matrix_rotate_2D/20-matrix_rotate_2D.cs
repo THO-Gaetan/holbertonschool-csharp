f2184b95f536ca2bbc64e0636f3eedabda56f90f
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 12:22:00 +0200

    exercice 20

--- csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        if (matrix.GetLength(0) != matrix.GetLength(1)
+        || (matrix.GetLength(0) != 2 && matrix.GetLength(0) != 3))
+            return new double[,] { { -1 } };
+        
+        double cos = Math.Round(Math.Cos(angle), 10);
+        double sin = Math.Round(Math.Sin(angle), 10);
+        double[,] rotation = {
+            { cos, -sin },
+            { sin,  cos }
+        };
+
+        int size = matrix.GetLength(0);
+        int inner = rotation.GetLength(1);
+        double[,] result = new double[size,size];
+
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                for (int k = 0; k < inner; k++)
+                    result[i, j] += rotation[i, k] * matrix[k, j];
+                    
+        return result;
+    }
+}
