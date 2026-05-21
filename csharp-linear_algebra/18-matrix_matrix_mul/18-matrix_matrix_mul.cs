875d675fe027012674df091dcf0bc3510066e78e
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 11:55:33 +0200

    adding exercice 18

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -10,13 +10,9 @@ class MatrixMath
         int rows = matrix1.GetLength(0);
         int cols = matrix1.GetLength(1);
         double[,] result = new double[rows, cols];
-        if (matrix1.GetLength == 2)
-        {
-            result[0,0] = (matrix1[0,0] x matrix2[0,0]) + (matrix1[0,1] * matrix2[1,0]);
-            result[0,1] = (matrix1[0,0] x matrix2[0,1]) + (matrix1[0,1] * matrix2[1,1]);
-            result[1,0] = (matrix1[1,0] x matrix2[0,0]) + (matrix1[1+1] * matrix2[1,0]);
-            result[1,1] = (matrix1[1,0] x matrix2[0,1]) + (matrix1[1+1] * matrix2[1,1]);
-        }
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i,j] = (matrix1[i,0] * matrix2[0,j] + (matrix1[i,1] * matrix2[1,j]))
         return result;
     }
 }
