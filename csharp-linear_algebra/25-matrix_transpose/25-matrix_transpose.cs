43bb48b55dbffb5de53cc1105002b3c33ad4ea30
THO-Gaetan <9771@holbertonstudents.com>
2026-05-21 13:07:51 +0200

    exercice 23

--- csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MatrixMath
+{
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+            return new double[,] {};
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        double[,] result = new double[cols, rows];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[j, i] = matrix[i, j];
+
+        return result;
+    }
+}
