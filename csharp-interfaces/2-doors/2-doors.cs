653bf414a689e0fca00301c2a596488a05f0410b
THO-Gaetan <9771@holbertonstudents.com>
2026-07-17 11:59:03 +0200

    first try for task 2

--- csharp-interfaces/2-doors/2-doors.cs
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+    override public string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+
+    public interface IInteractive
+    {
+        void Interact();
+    }
+
+    public interface iBreakable
+    {
+        int durability { get; set; }
+        void Break();
+    }
+
+    public interface ICollectable
+    {
+        bool isCollected { get; set; }
+        void Collect();
+    }
+}
+
+class Door : Base, Base.IInteractive
+{
+    public Door(string name)
+    {
+        this.name = name;
+    }
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
