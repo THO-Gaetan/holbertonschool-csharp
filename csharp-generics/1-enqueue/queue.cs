e65d1d91dcd77aa25a6feb0d957e6f0c1f2090ca
THO-Gaetan <9771@holbertonstudents.com>
2026-07-31 14:06:16 +0200

    pushing to test the ex 1 script

--- csharp-generics/1-enqueue/queue.cs
+++ csharp-generics/1-enqueue/queue.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+/// <summary>
+/// Represents a generic queue data structure.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+class Queue<T>
+{
+    public class Node
+    {
+        public T value { get; set; }
+        public Node Next { get; set; }
+        public Node head { get; set; }
+        public Node tail { get; set; }
+        public int count { get; set; }
+        public Node(T Value)
+        {
+            value = Value;
+            Next = null;
+        }
+    }
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public Node Enqueue(T value)
+    {
+        if (head == null)
+            head = new Node(value);
+        else
+        {
+            Node newNode = new Node(value);
+            tail.Next = newNode;
+            count++;
+        }
+        return tail;
+    }
+}
